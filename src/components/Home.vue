ecb151f545b923c890c27773ff21d17e04d0c4e5
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-02-16 14:37:07 +0900

    ホーム画面が見られるように

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="customer-page">
+    <h1>{{ page.fields.headline }}</h1>
+    <div v-html="page.fields.body"></div>
+  </div>
+</template>
+
+<script>
+import { butter } from '@/buttercms'
+export default {
+  name: 'top-page',
+  data () {
+    return {
+      page: {
+        fields: {}
+      }
+    }
+  },
+  methods: {
+    getPage () {
+      butter.page.retrieve('*', 'homepage')
+        .then((res) => {
+          console.log(res.data.data)
+          this.page = res.data.data
+        }).catch((res) => {
+          console.log(res)
+        })
+    }
+  },
+  created () {
+    this.getPage()
+  }
+}
+</script>
